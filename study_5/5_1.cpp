202125427883738d26febdc22fac14853d109dbc
Hanna Park <qkrgkssk8087@gmail.com>
2023-06-23 16:01:11 +0900

    study string commit!

--- study_5/5_1.cpp
+++ study_5/5_1.cpp
@@ -2,16 +2,16 @@
 #include <string>
 using namespace std;
 
-//int study_string_1();
-//int study_string_2();
-//int study_string_class();
+int study_string_1();
+int study_string_2();
+int study_string_class();
 int study_string_method();
 
 int main() {
 
-	//study_string_1();
-	//study_string_2();
-	//study_string_class();
+	study_string_1();
+	study_string_2();
+	study_string_class();
 	study_string_method();
 }
 
